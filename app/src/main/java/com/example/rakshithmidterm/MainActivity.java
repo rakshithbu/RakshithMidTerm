a77d035be4d23beebfad1880cb3aac56006a3502
rakshith <rakshithbu92@gmail.com>
2020-04-09 12:15:28 -0400

    Created android project for midterm test

--- app/src/main/java/com/example/rakshithmidterm/MainActivity.java
+++ app/src/main/java/com/example/rakshithmidterm/MainActivity.java
@@ -6,22 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener, View.OnClickListener{
 
     RadioGroup rg;
     String checkedButtonId = "";
-    TextView fee,hours, welcome;
+    TextView fee,hours, welcome,totalFee,totalHours;
     Spinner sp;
     String [] courses ={"Java","Swift","IOS","Android","Database"};
     int [] courseHours ={6,5,5,7,4};
     int [] coursefees = {1300, 1500, 1350, 1400, 1000};
     int totalCourseFees = 0;
     int totalCourseHours = 0;
+    int pos =0;
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +36,16 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         fee=findViewById(R.id.fee);
         hours=findViewById(R.id.hours);
         welcome = findViewById(R.id.welcome);
-       // welcome.setText("welcome "+ login.name.getText())
+        totalFee = findViewById(R.id.totalFee);
+        totalHours = findViewById(R.id.totalHours);
+        welcome.setText("welcome  "+ login_activity.name.getText());
         sp=findViewById(R.id.spCourse);
         ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,courses);
         aa.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         sp.setAdapter(aa);
         sp.setOnItemSelectedListener(this);
+        add = findViewById(R.id.add);
+        add.setOnClickListener(this);
     }
 
 
@@ -57,9 +65,9 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         fee.setText(String.valueOf(coursefees[position]));
         hours.setText(String.valueOf(courseHours[position]));
 
+        pos = position;
 
-        totalCourseFees = totalCourseFees+ coursefees[position];
-        totalCourseHours = totalCourseHours + courseHours[position];
+
     }
 
     @Override
@@ -69,6 +77,34 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(checkedButtonId.equalsIgnoreCase("Grad")){
+            int tempHours = totalCourseFees +  courseHours[pos];
+            if(tempHours >21){
+                Toast.makeText(getApplicationContext(), "Cannot have total hours more than 21", Toast.LENGTH_LONG).show();
+            }else{
+                totalCourseFees = totalCourseFees+ coursefees[pos];
+                totalCourseHours = totalCourseHours + courseHours[pos];
+            }
+            totalHours.setText(String.valueOf(totalCourseHours));
+            totalFee.setText(String.valueOf(totalCourseFees));
+
+        }else{
+            int tempHours = totalCourseFees +  courseHours[pos];
+                if(tempHours >19){
+                Toast.makeText(getApplicationContext(), "Cannot have total hours more than 19 for undergraduate", Toast.LENGTH_LONG).show();
+            }else{
+                    totalCourseFees = totalCourseFees+ coursefees[pos];
+                    totalCourseHours = totalCourseHours + courseHours[pos];
+                }
+
+            totalHours.setText(String.valueOf(totalCourseHours));
+            totalFee.setText(String.valueOf(totalCourseFees));
+        }
 
     }
 }
